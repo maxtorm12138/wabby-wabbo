68b66a4f315e25aae68ea54fc360220d30d67e90
maxzwliao <maxzwliao@tencent.com>
2022-05-16 18:37:32 +0800

    buffer

---
+++ src/render/vulkan/vk_buffer.cpp
@@ -0,0 +1,69 @@
+#include "vk_buffer.hpp"
+
+// fmt
+#include "fmt/format.h"
+
+namespace wabby::render::vulkan
+{
+
+buffer::buffer(const vk_device_allocator &allocator, vk::BufferCreateInfo buffer_create_info, VmaAllocationCreateInfo allocation_create_info) :
+    allocator_(allocator)
+{
+    VkBuffer buffer;
+    auto result = vmaCreateBuffer(
+        allocator_.get(),
+        reinterpret_cast<vk::BufferCreateInfo::NativeType *>(&buffer_create_info),
+        &allocation_create_info,
+        &buffer,
+        &allocation_,
+        &allocation_info_);
+
+    if (result != VK_SUCCESS) 
+    {
+        throw std::runtime_error(fmt::format("vmaCreateBuffer fail result: {}", result));
+    }
+    buffer_ = vk::Buffer(buffer);
+}
+
+buffer::buffer(buffer &&other) noexcept :
+    allocator_(other.allocator_),
+    buffer_(std::exchange(other.buffer_, buffer_)),
+    allocation_(std::exchange(other.allocation_, allocation_)),
+    allocation_info_(std::exchange(other.allocation_info_, allocation_info_))
+{
+}
+
+buffer &buffer::operator=(buffer &&other) noexcept
+{
+    allocator_ = other.allocator_;
+    std::swap(this->buffer_, other.buffer_);
+    std::swap(this->allocation_, other.allocation_);
+    std::swap(this->allocation_info_, other.allocation_info_);
+    return *this;
+}
+
+buffer::~buffer() noexcept
+{
+    if (allocation_ != VK_NULL_HANDLE)
+    {
+        vmaDestroyBuffer(allocator_.get(), buffer_, allocation_);
+    }
+}
+
+void *buffer::map() const
+{
+    void *data;
+    auto result = vmaMapMemory(allocator_.get(), allocation_, &data);
+    if (result != VK_SUCCESS)
+    {
+        throw std::runtime_error(fmt::format("vmaMapMemory fail result: {}", result));
+    }
+    return data;
+}
+
+void buffer::unmap() const
+{
+    vmaUnmapMemory(allocator_.get(), allocation_);
+}
+
+}
---
+++ src/render/vulkan/vk_buffer.hpp
@@ -0,0 +1,43 @@
+#ifndef _WABBY_VULKAN_BUFFER_HPP
+#define _WABBY_VULKAN_BUFFER_HPP
+
+// module
+#include "vk_device_allocator.hpp"
+
+// boost
+#include "boost/noncopyable.hpp"
+
+// vma
+#include "vk_mem_alloc.h"
+
+// vulkan
+#include "vulkan/vulkan.hpp"
+
+namespace wabby::render::vulkan
+{
+
+class buffer : public boost::noncopyable
+{
+public:
+    buffer(const vk_device_allocator &allocator, vk::BufferCreateInfo buffer_create_info, VmaAllocationCreateInfo allocation_create_info);
+
+    buffer(buffer &&other) noexcept;
+
+    buffer &operator=(buffer &&other) noexcept;
+
+    ~buffer() noexcept;
+
+public:
+    void *map() const;
+
+    void unmap() const;
+
+private:
+    std::reference_wrapper<const vk_device_allocator> allocator_;
+    VmaAllocation allocation_{VK_NULL_HANDLE};
+    VmaAllocationInfo allocation_info_;
+    vk::Buffer buffer_;
+};
+}
+
+#endif
