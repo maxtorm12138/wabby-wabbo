a00e44b506acd201196bc05f3f628f02980ec358
maxzwliao <maxzwliao@tencent.com>
2022-05-25 19:49:19 +0800

    fix: compile

--- include/wabby/container/delayed.hpp
+++ include/wabby/container/delayed.hpp
@@ -10,65 +10,53 @@
 
 namespace wabby::container
 {
-  namespace detail
-  {
-    class delayed_tracer
-    {
-    protected:
-      void test_constructed();
-
-      void test_not_constructed();
-
-      void set_constructed();
-
-    protected:
-#ifndef NDEBUG
-      static std::unordered_map<void *, bool> constructed_;
-#endif
-    };
-  }  // namespace detail
-
   template <typename T>
-  class delayed : public detail::delayed_tracer
+  class delayed
   {
   public:
     delayed(){};
 
     ~delayed()
     {
-      test_constructed();
-      std::destroy_at( reinterpret_cast<T *>( memory_.data() ) );
+      if ( constructed_ )
+      {
+        std::destroy_at( reinterpret_cast<T *>( memory_.data() ) );
+      }
     }
 
   public:
     template <typename... Args>
     void construct( Args &&... args )
     {
-      test_not_constructed();
+      if ( constructed_ )
+      {
+        std::destroy_at( reinterpret_cast<T *>( memory_.data() ) );
+      }
       std::construct_at( reinterpret_cast<T *>( memory_.data() ), std::forward<Args>( args )... );
-      set_constructed();
+      constructed_ = true;
     }
 
     T * operator->()
     {
-      test_constructed();
+      assert( constructed_ && "object shold be constructed" );
       return reinterpret_cast<T *>( memory_.data() );
     }
 
     T & operator*()
     {
-      test_constructed();
+      assert( constructed_ && "object shold be constructed" );
       return *( reinterpret_cast<T *>( memory_.data() ) );
     }
 
     operator T &()
     {
-      test_constructed();
+      assert( constructed_ && "object shold be constructed" );
       return *( reinterpret_cast<T *>( memory_.data() ) );
     }
 
   private:
     std::array<uint8_t, sizeof( T )> memory_{};
+    bool                             constructed_{ false };
   };
 
 }  // namespace wabby::container
--- src/container/delayed.cpp
+++ src/container/delayed.cpp
@@ -2,26 +2,4 @@
 
 namespace wabby::container::detail
 {
-  std::unordered_map<void *, bool> delayed_tracer::constructed_;
-
-  void delayed_tracer::test_constructed()
-  {
-#ifndef NDEBUG
-    assert( constructed_[this] && "object shold be constructed" );
-#endif  // !NDEBUG
-  }
-
-  void delayed_tracer::test_not_constructed()
-  {
-#ifndef NDEBUG
-    assert( !constructed_[this] && "construct shold not be constructed" );
-#endif  // !NDEBUG
-  }
-
-  void delayed_tracer::set_constructed()
-  {
-#ifndef NDEBUG
-    constructed_[this] = true;
-#endif  // !NDEBUG
-  }
 }  // namespace wabby::container::detail
